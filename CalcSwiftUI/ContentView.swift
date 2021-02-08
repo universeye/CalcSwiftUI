04391609f5fad09bb2c49c3a9a9e87d4732bdfee
Terry <terrykuo501@gmail.com>
2021-02-08 16:20:05 +0800

    Initial Commit

---
+++ CalcSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalcSwiftUI
+//
+//  Created by Terry Kuo on 2021/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
